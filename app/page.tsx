c889b4c58bc16714d013d593acf80e186e244693
Pandelis Zembashis <pandelis.zembashis@cosine.sh>
2025-04-16 20:52:19 +0000

    feat: enable client-side rendering for ChessGame component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import dynamic from "next/dynamic";
 // Use dynamic to enable 'use client' ChessGame component in Next.js 13+ app dir
 const ChessGame = dynamic(() => import("./ChessGame"), { ssr: false });
